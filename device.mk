bd2d91543c187eb72a161bcf7e480ed6d0089a4b
Bruno Martins <bgcngm@gmail.com>
2019-09-30 13:32:04 +0100

    passion: Drop mediaextractor seccomp policy
    These syscalls are now allowed by default.
    
    [arco] Also clean mediacodec.policy
    
    Change-Id: I405f609d030331e19031612cbd912a54a3c72339

--- device.mk
+++ device.mk
@@ -443,8 +443,7 @@ PRODUCT_PACKAGES += \
 
 # Seccomp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # Security configuration file
 PRODUCT_COPY_FILES += \
