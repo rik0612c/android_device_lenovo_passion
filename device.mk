effab1cbc64c55d46322a8711ab5fbd20ae157bd
chrmhoffmann <chrmhoffmann@gmail.com>
2020-02-02 12:53:51 +0100

    passion: Drop external camera config
    Change-Id: If6e611bbb7897bfbcdba5e381c62348d8d91373c

--- device.mk
+++ device.mk
@@ -117,10 +117,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
     camera.device@1.0-impl-legacy
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
-
+    
 # Connectivity Engine
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.cne.dpm=0 \
