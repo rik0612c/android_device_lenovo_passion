7188cb1569975a98452aa52111ce22ada1fc3bff
LuK1337 <priv.luk@gmail.com>
2022-07-03 12:27:04 +0000

    passion: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172
    Signed-off-by: RahifM <faizel326@gmail.com>

--- device.mk
+++ device.mk
@@ -385,6 +385,12 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0-java
+    
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # Permissions
 PRODUCT_COPY_FILES += \
